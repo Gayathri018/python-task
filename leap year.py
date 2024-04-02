809ea73b5036289a5ad5a358d0dc2709433e951d
Gayathri018 <163122362+Gayathri018@users.noreply.github.com>
2024-04-02 11:12:34 +0530

    Add files via upload

--- leap year.py
+++ leap year.py
@@ -1,10 +1,9 @@
 n=int (input("enter the year"))
-if(n%4==0 and n%100!=0):{
-print("leap year")
-}
-elif(n%100==0 and n%400==0):{
-print("leap year")
-}
-else: {
-print("not a leap year")
-}
+if(n%4==0 and n%100!=0):
+    print("leap year not a century year")
+
+elif(n%400==0 and n%100==0):
+    print("leap year and is a century year")
+
+else: 
+    print("not a leap year")
